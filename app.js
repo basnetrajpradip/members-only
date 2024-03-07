8f0a4332dc7c9d21baf9ec9293beee2f51a6c892
basnetrajpradip <basnetrajpradip@gmail.com>
2024-03-07 13:44:12 +0545

    Set up required configs for the app

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ dotenv.config();
 const app = express();
 
 // Set up rate limiter: maximum of twenty requests per minute
-const RateLimit = require("express-rate-limit");
 const limiter = RateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
   max: 20,
